fcf106eacd1bfc97ea9a12cb1dcb8a58a7ad9c89
Juan Echavarria <juanea@gmail.com>
2026-06-06 08:19:05 -0700

    botwheel lyrical

--- odrive_botwheel_explorer/launch/botwheel_explorer.launch.py
+++ odrive_botwheel_explorer/launch/botwheel_explorer.launch.py
@@ -106,7 +106,13 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["botwheel_explorer", "--controller-manager", "/controller_manager"],
+        arguments=[
+            "botwheel_explorer",
+            "--param-file",
+            robot_controllers,
+            "--controller-manager",
+            "/controller_manager",
+        ],
     )
 
 #    # Delay rviz start after `joint_state_broadcaster`
